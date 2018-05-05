1298844e9a8b430442fff5a041c76b1d15346939
stivens73 <stivens54@gmail.com>
2018-05-04 20:34:11 -0700

    Populated Event

--- src/Event.java
+++ src/Event.java
@@ -1,2 +1,83 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Event {
+
+    private Calendar calendar;
+
+    private String title;
+    private String time;
+    SimpleDateFormat format;
+
+    private Integer eventBegins;
+    private Integer eventEnds;
+
+    public static final String FORMAT1 = "MM/dd/yyyy";
+    public static final String FORMAT2 = "MM/dd/yyyy:HH:mm";
+
+    public Event(String [] event) {
+
+        calendar = new GregorianCalendar();
+        format = new SimpleDateFormat( FORMAT1 );
+
+        try { calendar.setTimeInMillis( Long.parseLong( event[0] ) ) ; title = event[1];  time = event[2]; setTime(event[2]); }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Event(String _title, String _date, String _time, long _id) {
+
+
+        calendar = new GregorianCalendar();
+        format = new SimpleDateFormat( FORMAT1 );
+
+        calendar.setTimeInMillis(_id);
+        title = _title;
+        time = _time;
+//        date = _date;
+
+        setTime(_time);
+    }
+
+    private void setTime(String time) {
+        String temp [] = time.split("-");
+        eventBegins = Integer.parseInt( temp[0].replaceAll(":", "") );
+        try {
+            eventEnds = Integer.parseInt( temp[1].replaceAll(":", "") );
+        } catch (Exception e) {
+            eventBegins = 0;
+        }
+
+    }
+
+    public Integer getEventBegins() {
+        return eventBegins;
+    }
+
+    public Integer getEventEnds() {
+        return eventEnds;
+    }
+
+    public String getDate() {
+        return format.format(calendar.getTime());
+    }
+
+    public boolean checkConflict(String time) {
+        if(eventEnds == 0) return false;
+        String temp [] = time.split("-");
+        int a, b;
+        a = Integer.parseInt( temp[0].replaceAll(":", "") );
+        try {
+            b = Integer.parseInt( temp[1].replaceAll(":", "") );
+            if(b == 0) {
+                return (a > eventBegins && a < eventEnds);
+            } else return (a > eventBegins && a < eventEnds) || (b > eventBegins && b < eventEnds);
+        } catch (Exception e) {
+            return (a > eventBegins && a < eventEnds);
+        }
+    }
+
 }
--- src/StudentCalendar.java
+++ src/StudentCalendar.java
@@ -1,13 +1,12 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
 import java.util.Calendar;
 
 public class StudentCalendar extends JPanel {
 
     enum MONTHS {
-        Jan, Feb, March, Apr, May, June, July, Aug, Sep, Oct, Nov, Dec
+        January, February, March, April, May, June, July, August, September, October, November, December
     }
 
     enum DAYS {
@@ -20,39 +19,130 @@ public class StudentCalendar extends JPanel {
 
         model = new Model();
 
+//        setLayout(new GridBagLayout());
+        setBackground(Color.WHITE);
         setLayout(new BorderLayout());
 
         init();
     }
 
-    public void init() { //Graphics2D g2) {
+    public void init() {
 
-        JPanel leftPanel = new JPanel(new BorderLayout());
-//        JPanel leftPanel = new JPanel(new FlowLayout());
-        JPanel monthView = new JPanel(new GridLayout(6, 6, 1, 1));
-
-//        Shape shape = new Rectangle2D.Double(0, 0, 100, 100);
-//        add(shape, BorderLayout.SOUTH);
-        setMonthView(monthView);
-        monthView.setPreferredSize(new Dimension(150, 150));
-
-//        JButton button = new JButton("CREATE");
-//        button.setSize(new Dimension(50, 30));
-//        button.setBackground(Color.RED);
-//        button.setForeground(Color.GRAY);
-//        button.setOpaque(true);
-
-        JLabel button = getButton("CREATE", Color.RED, Color.WHITE);
-
-//        leftPanel.setSize(new Dimension(150, 80));
-        leftPanel.add(button  , BorderLayout.NORTH );
-        leftPanel.add(monthView    , BorderLayout.CENTER );
+        GridBagConstraints c = new GridBagConstraints();
 
         JPanel upperPanel = new JPanel(new FlowLayout());
         upperPanel.setPreferredSize(new Dimension( 700, 50));
+        JLabel placeHolder = new JLabel("PLACEHOLDER");
+        upperPanel.add(placeHolder);
+//        c.weightx = 0.5;
+//        c.gridx = 0;
+//        c.gridy = 0;
+//        add(upperPanel, c);
 
-        add(leftPanel, BorderLayout.WEST);
+
+        JPanel leftPanel = new JPanel();
+//        JPanel leftPanel = new JPanel(new GridBagLayout());
+        setLeftViewPanel(leftPanel);
+//        c.fill = GridBagConstraints.EAST;
+//        c.weightx = 0.5;
+//        c.gridx = 1;
+//
+//        c.gridy = 0;
+//        add(leftPanel, c);
+
+        JPanel eventsPanel = new JPanel(new GridLayout(6, 1));
+//        JPanel eventsPanel = new JPanel();
+        setEventsPanel(eventsPanel);
+
         add(upperPanel, BorderLayout.NORTH);
+        add(leftPanel, BorderLayout.WEST);
+        add(eventsPanel, BorderLayout.CENTER);
+    }
+
+    public void setEventsPanel(JPanel eventsPanel) {
+
+//        eventsPanel.setLayout(new BoxLayout(eventsPanel, BoxLayout.Y_AXIS));
+//        eventsPanel.setBackground(Color.lightGray);
+        JScrollPane scrollPane = new JScrollPane(eventsPanel);
+        JPanel eventHolder = new JPanel();
+//        eventHolder.setLayout(new BoxLayout(eventHolder, BoxLayout.Y_AXIS));
+
+        for(int c = 0; c < 6; c++) {
+//            eventHolder.add();
+//            eventsPanel.add(new JSeparator());
+            JLabel event = new JLabel(c + 5 + " am");
+            event.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            eventsPanel.add(event);
+            eventsPanel.add(new JLabel("PLACEHOLDER"));
+        }
+
+        eventsPanel.setLayout(new GridLayout(6, 0));
+//        eventsPanel.setSize();
+
+        eventsPanel.setMinimumSize(new Dimension(700 - 150, 150));
+        eventsPanel.setPreferredSize(new Dimension(700 - 150, 150));
+        eventsPanel.setMaximumSize(new Dimension(700 - 150, 500));
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public void setLeftViewPanel(JPanel leftPanel) {
+
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+        GridBagConstraints c = new GridBagConstraints();
+        JPanel monthView = new JPanel(new GridLayout(6, 6, 1, 1));
+
+        setMonthView(monthView);
+        monthView.setPreferredSize(new Dimension(150, 150));
+
+//        JPanel buttonHolder = new JPanel(new BorderLayout());
+//        JLabel button = getButton("CREATE", Color.RED, Color.WHITE);
+//        buttonHolder.add(button, BorderLayout.CENTER);
+//        buttonHolder.setSize(new Dimension(40, 20));
+
+
+        JButton button = new JButton("CREATE");
+        button.setBackground(Color.RED);
+        button.setForeground(Color.WHITE);
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+//        button.setHorizontalAlignment(Component.LEFT_ALIGNMENT);
+
+        button.addActionListener(e -> System.out.println("Create pressed"));
+
+//        buttonHolder.setBorder(BorderFactory.createEmptyBorder(0,40,0,40));
+
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.weightx = 0.5;
+//        c.gridwidth = 1;
+//        c.gridx = 0;
+//        c.gridy = 0;
+//        c.ipady = 40;
+//        c.ipadx = 10;
+
+//        leftPanel.add(buttonHolder, c);
+
+        leftPanel.add(button);
+
+        JLabel monthYear = new JLabel( MONTHS.values()[model.getCal().get(Calendar.MONTH)] + " " + model.getCal().get(Calendar.YEAR) );
+        monthYear.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.weightx = 0.5;
+//        c.gridx = 0;
+//        c.gridy = 1;
+//        leftPanel.add(monthYear, c);
+
+        leftPanel.add(monthYear);
+
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.weightx = 0.5;
+//        c.gridx = 0;
+//        c.gridy = 2;
+//        leftPanel.add(monthView, c);
+
+        leftPanel.add(monthView);
     }
 
     public void setMonthView(JPanel monthView) {
@@ -77,24 +167,14 @@ public class StudentCalendar extends JPanel {
         }
     }
 
-    public JLabel getButton(String text, Color background, Color foregroung) { //}, int x, int y) {
+    public JLabel getButton(String text, Color background, Color foregroung) {
 
         JLabel button = new JLabel(text);
         button.setBackground(background);
         button.setForeground(foregroung);
-        button.setSize(new Dimension(150, 20));
+        button.setSize(new Dimension(200, 40));
         button.setHorizontalAlignment(SwingConstants.CENTER);
         button.setOpaque(true);
         return button;
-//        g2.draw(new Rectangle2D.Double(x, y, 100, 20));
     }
-//
-//    public void paintComponent(Graphics g) {
-//
-//        super.paintComponent(g);
-//
-//        Graphics2D g2 = (Graphics2D) g;
-//
-//        init(g2);
-//    }
 }
